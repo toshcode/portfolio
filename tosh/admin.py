870547328df569118060a9f40767c32d95187d0a
toshcode <gitonjeruh@gmail.com>
2021-12-07 19:30:27 +0300

    register models to the admin dashboard

--- tosh/admin.py
+++ tosh/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Education, Experience, Skillset, Project, Message, Information
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Information)
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Skillset)
+admin.site.register(Project)
+admin.site.register(Message)
